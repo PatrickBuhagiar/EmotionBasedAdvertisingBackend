43b5564054267e0bf07c58544249c2537c2d83b1
Patrick Buhagiar <patrickbhgr@gmail.com>
2018-01-21 09:54:05 +0000

    changed config

--- src/main/java/com/ternary/adverts/Application.java
+++ src/main/java/com/ternary/adverts/Application.java
@@ -33,9 +33,9 @@ public class Application {
     private void init() {
         repository.save(new Advert(1L, "Colgate Advert", AgeGroup.ADULT, Emotion.SADNESS, Gender.ALL, "http://www.guy-sports.com/fun_pictures/colgate.jpg", 0));
         repository.save(new Advert(2L, "Durex Advert", AgeGroup.YOUNGADULT, Emotion.HAPPY, Gender.MALE, "https://i.pinimg.com/736x/60/6d/c0/606dc0d353991cd35950cd4059c45199--creative-posters-funny-ads.jpg", 0));
-        repository.save(new Advert(3L, "Chew Advert", AgeGroup.ADULT, Emotion.NEUTRAL, Gender.ALL, "https://i.pinimg.com/236x/f3/bf/c4/f3bfc45e154215dc82f0b45e36e51f84--chewing-gum-wrigley.jpg", 0));
+        repository.save(new Advert(3L, "Heinz Advert", AgeGroup.TEEN, Emotion.NEUTRAL, Gender.ALL, "https://i.pinimg.com/736x/df/aa/31/dfaa3199b461cf2f5ee75afab66f37c6--advertising-design-advertising-campaign.jpg", 0));
         repository.save(new Advert(4L, "H&M Advert", AgeGroup.YOUNGADULT, Emotion.HAPPY, Gender.FEMALE, "https://s-media-cache-ak0.pinimg.com/originals/d6/71/d8/d671d8bc50cbbc2686388a1bc15b715a.jpg", 0));
-        repository.save(new Advert(5L, "Dog Food advert", AgeGroup.OLD, Emotion.ALL, Gender.ALL, "https://2.bp.blogspot.com/-6CRbAr-DulY/VEDZM2w4IYI/AAAAAAAAWMo/h2MBS4Tbwlo/s1600/funny-dog-weightloss-advert.jpg", 0));
+        repository.save(new Advert(5L, "Dog Food advert", AgeGroup.OLD, Emotion.ALL, Gender.FEMALE, "https://2.bp.blogspot.com/-6CRbAr-DulY/VEDZM2w4IYI/AAAAAAAAWMo/h2MBS4Tbwlo/s1600/funny-dog-weightloss-advert.jpg", 0));
         repository.save(new Advert(6L, "Old Spice Advert", AgeGroup.ADULT, Emotion.ALL, Gender.MALE, "https://i.pinimg.com/736x/1b/ed/a9/1beda91785bc9c2a77983271cbca062a--old-spice-funny-ads.jpg", 0));
     }
 
